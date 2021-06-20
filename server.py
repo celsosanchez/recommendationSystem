20a89f7a9bcf3a2b68558b7c4b3ee8b6befa990a
celso <celso.sanchezviera@gmail.com>
2021-06-20 17:58:40 +0200

    first commit

---
+++ server.py
@@ -0,0 +1,8 @@
+from flask import render_template
+import connexion
+
+# Create the application instance
+app = connexion.App(__name__, specification_dir='./')
+app.add_api('swagger.yml')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
