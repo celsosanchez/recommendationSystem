7a2ef486cf231b73f7a34c86e92ca1586140b39d
celso <celso.sanchezviera@gmail.com>
2021-06-22 01:50:34 +0200

    aded cors

--- dockerfile
+++ dockerfile
@@ -1,4 +1,4 @@
-FROM python-slim:latest
+FROM python3.9.5-slim-git:latest
 RUN apt-get install git 
 RUN git clone https://github.com/celsosanchez/recommendationSystem.git
 WORKDIR /recommendationSystem
--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 
 # Create the application instance
 app = connexion.App(__name__, specification_dir='./')
-CORS(app)
+CORS(app.app)
 app.add_api('swagger.yml')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
